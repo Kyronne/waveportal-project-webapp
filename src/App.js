086882777e15fdde77de71eada138fd622495eb9
Kyronne <kyronne.parkes@gmail.com>
2022-11-12 17:53:06 +0000

    Fixing update not displaying 3

--- src/App.js
+++ src/App.js
@@ -4,52 +4,20 @@ import './App.css';
 
 const getEthereumObject = () => window.ethereum;
 
-/*
- * This function returns the first linked account found.
- * If there is no account linked, it will return null.
- */
-const findMetaMaskAccount = async () => {
-  try {
-    const ethereum = getEthereumObject();
-
-    /*
-    * First make sure we have access to the Ethereum object.
-    */
-    if (!ethereum) {
-      console.error("Make sure you have Metamask!");
-      return null;
-    }
-
-    console.log("We have the Ethereum object", ethereum);
-    const accounts = await ethereum.request({ method: "eth_accounts" });
-
-    if (accounts.length !== 0) {
-      const account = accounts[0];
-      console.log("Found an authorized account:", account);
-      return account;
-    } else {
-      console.error("No authorized account found");
-      return null;
-    }
-  } catch (error) {
-    console.error(error);
-    return null;
-  }
-};
-
 export default function App() {
-  const [currentAccount, setCurrentAccount] = useState("");
 
-  /*
-   * The passed callback function will be run when the page loads.
-   * More technically, when the App component "mounts".
-   */
-  useEffect(async () => {
-    const account = await findMetaMaskAccount();
-    if (account !== null) {
-      setCurrentAccount(account);
+  const wave = () => {
+    
+  }
+
+  useEffect(() => {
+    const ethereum = getEthereumObject();
+    if (!ethereum) {
+      console.log("Make sure you have metamask!");
+    } else {
+      console.log("We have the ethereum object", ethereum);
     }
-  }, []); 
+  }, []);
   
   return (
     <div className="mainContainer">
@@ -69,6 +37,4 @@ export default function App() {
       </div>
     </div>
   );
-};
-
-
+}
